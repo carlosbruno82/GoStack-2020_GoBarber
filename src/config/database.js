90e332693c495e6073542281bd359bec71dd0b5d
Carlos Bruno <carlao.linux@gmail.com>
2020-02-19 11:08:20 -0300

    Finalizado

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 module.exports = {
-  dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "docker",
-  database: "gobarber",
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll: true
-  }
+    underscoredAll: true,
+  },
 };
