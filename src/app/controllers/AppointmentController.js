645d22d8029072e9fbd0028041fa0a259b38f26f
Carlos Bruno <carlao.linux@gmail.com>
2020-02-27 16:03:02 -0300

    validacoes de agendamento

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,44 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    /**
+     * check if provider_id is a provider
+     */
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    /**
+     * check user equal provider
+     */
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'User cannot be same as providers' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
