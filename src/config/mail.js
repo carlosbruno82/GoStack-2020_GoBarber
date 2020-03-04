78d2f91713d09f5d4e1865f49d4894c89b6b5233
Carlos Bruno <carlao.linux@gmail.com>
2020-03-04 16:00:50 -0300

    Configurando fila com Redis

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'dc80802b32b658',
+    pass: '7df447311514ba',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
